5c8d29b787d97819f49b01702badd6851cdc1ec3
lemunao <pedroandres1@gmail.com>
2022-07-29 23:19:59 -0400

    Added delete, and add on Javascript

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,35 +1,91 @@
 var tareas = [
     { id: 1, tarea: "ir a Supermercado", completada: false },
     { id: 2, tarea: "Barrer", completada: false },
-    { id: 3, tarea: "Cocinar", completada: true }
+    { id: 3, tarea: "Cocinar", completada: false }
 ]
 
 var htmlTask = document.querySelector("#tasks");
 var htmlTaskCompleted = document.querySelector("#taskDone");
+let clickButton = document.querySelector("#submitTask");
+var taskList = document.querySelector("#taskList");
 
-let pendingTask = tareas.forEach(x => x = tareas.length )
-htmlTask.innerHTML = pendingTask
+let htmlUpdate = ""
+
+// Create initial list
+for (let tarea of tareas) {
+    htmlUpdate += ` <div class="taskList"> 
+                        <div> ${tarea.id}</div>
+                        <div>${tarea.tarea}</div>
+                        <div><input type="checkbox" id="checkClick" onclick="changeStatus(${tarea.id})"></input></div>
+                        <div><button onclick="deleteElement(${tarea.id})">X</button></div>
+                    </div>`
+}
+taskList.innerHTML = htmlUpdate
+
+// Add new item on list
+clickButton.onclick = () => {
+    let newItem = document.querySelector("#newTask").value
+    let idCreator = 0;
+    //Create new ID
+    for (tarea of tareas) {
+        idCreator = tarea["id"] + 1
+    }
+    //Create new item on array
+    tareas.push({ id: idCreator, tarea: newItem, completada: false })
+    htmlUpdate = ""
+    for (let tarea of tareas) {
+        htmlUpdate += ` <div class="taskList"> 
+                            <div> ${tarea.id}</div>
+                            <div>${tarea.tarea}</div>
+                            <div><input type="checkbox" id="checkClick" onclick="changeStatus(${tarea.id})"></input></div>
+                            <div><button onclick="deleteElement(${tarea.id})">X</button></div>
+                        </div>`
+    }
+    console.log(tareas)
+    taskList.innerHTML = htmlUpdate
+    htmlTask.innerHTML = `Tareas Totales: ${tareas.length}`
+    const pendingTask = tareas.filter(task => task.completada == false)
+    htmlTaskCompleted.innerHTML = `Tareas Pendientes: ${pendingTask.length}`
+}
+
+//Change status to done
+function changeStatus(id) {
+    const index = tareas.findIndex((task) => task.id == id)
+    checkClick = document.querySelector("#checkClick")
+    if (checkClick.checked) {
+
+    } else {
+        tareas.completada == false;
+        console.log(index)
+    }
+}
 
 
+//Delete item from list
+function deleteElement(id) {
+    console.log(id)
+    const index = tareas.findIndex((del) => del.id == id)
+    tareas.splice(index, 1)
+    let htmlUpdate = ""
+    for (let tarea of tareas) {
+        htmlUpdate += ` <div class="taskList"> 
+                            <div> ${tarea.id}</div>
+                            <div>${tarea.tarea}</div>
+                            <div><input type="checkbox" id="checkClick" onclick="changeStatus(${tarea.id})"></input></div>
+                            <div><button onclick="deleteElement(${tarea.id})">X</button></div>
+                        </div>`
+    }
+    taskList.innerHTML = htmlUpdate;
+    htmlTask.innerHTML = `Tareas Totales: ${tareas.length}`
+    const pendingTask = tareas.filter(task => task.completada == false)
+    htmlTaskCompleted.innerHTML = `Tareas Pendientes: ${pendingTask.length}`
+}
 
+htmlTask.innerHTML = `Tareas Totales: ${tareas.length}`
+const pendingTask = tareas.filter(task => task.completada == false)
+htmlTaskCompleted.innerHTML = `Tareas Pendientes: ${pendingTask.length}`
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
 (() => {
     'use strict'
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
@@ -41,8 +97,8 @@ htmlTask.innerHTML = pendingTask
                 event.preventDefault()
                 event.stopPropagation()
             }
-
             form.classList.add('was-validated')
         }, false)
     })
 })()
+*/
